9e9682142872b177b329adcd84f031fbd0a0bae5
Carlos <juancarlosgomezpacheco283@gmail.com>
2025-03-21 17:26:24 -0600

    Se adapto la pagina home para la parte de los post

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
